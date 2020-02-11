d6a9371318fea5228c1d8cfa2e3b920cc04cb490
cyn-mor26 <60100238+cyn-mor26@users.noreply.github.com>
2020-02-10 23:17:04 -0300

    terminado

--- Automovil_nuevo.java
+++ Automovil_nuevo.java
@@ -1,55 +1,48 @@
-package Automovil;
+package Ejercicioprueba3;
 
 public class Automovil_nuevo {
-	//atributos de automovil nuevo
-		private String marca;
-		private String modelo ;
-		private int año;
-		private double precio;
-		
-		//constructor
-		public Automovil_nuevo (String marca, String modelo ,int año,double precio ){
-			
-		}
-
-//metodos get y set de cada atributo
-		public String getMarca() {
-			return marca;
-		}
-
-		public void setMarca(String marca) {
-			this.marca = marca;
-		}
-
-		public String getModelo() {
-			return modelo;
-		}
-
-		public void setModelo(String modelo) {
-			this.modelo = modelo;
-		}
-
-		public int getAño() {
-			return año;
-		}
-
-		public void setAño(int año) {
-			this.año = año;
-		}
-
-		public double getPrecio() {
-			return precio;
-		}
-
-		public void setPrecio(double precio) {
-			this.precio = precio;
-		}
-		public double  preciofinal() {
-			return this.precio*1.19*1.05+100000;
-		}
+	private String marca;
+	private String modelo ;
+	private int año;
+	private double precio;
 	
-@Override
-public String toString() {
-	return "Automovil_nuevo [marca=" + marca + ", modelo=" + modelo + ", año=" + año + ", precio =" + precio+ "]";
-}
-}
+	public Automovil_nuevo (String marca, String modelo ,int año,double precio ){
+		
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public double preciofinal(double preciofinal,double interes,double comisión,int patente ) {
+		 precio=(precio*1.19*1.05)+100000;
+		 return preciofinal;
+	}
+	}

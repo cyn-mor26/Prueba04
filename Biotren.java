b79bedf4d57bedb3cb27d100c8f2baee053ee81d
cyn-mor26 <60100238+cyn-mor26@users.noreply.github.com>
2020-02-11 10:20:27 -0300

    Update Biotren.java

--- Biotren.java
+++ Biotren.java
@@ -10,16 +10,16 @@ public class Biotren {
 	private boolean activa;
 	
 	public void abonoDinero(int abono) {
-		
+		//Si abono es mayor a cero 
 		if (abono > 0) {
-			
+			//sumar abono a saldo
 			this.saldo = saldo + abono;
-			System.out.println("Se han cargado $" + abono + " a su tarjeta. "
-							   + "Su nuevo saldo es de $" + saldo + ".");
+			//Me imprime lo abonado y el  saldo final
+			System.out.println("Se han cargado $" + abono + " a su tarjeta. " + "Su nuevo saldo es de $" + saldo + ".");
 			
 		}
 		
-		
+		//si el abono es mayor a 500 , activa es verdadero , por lo tanto se reactiva tarjeta.
 		if (abono >= 500 && !(activa)) {
 			
 			activa = true;
@@ -28,13 +28,13 @@ public class Biotren {
 		}
 		
 	}
-	
+	//metodo para tarifa normal o estudiante
 	public void asignarTarifa() {
-		
+		// si la tarjeta de usuario es TNE , la tarifa sera de 200
 		if (t_Usuario == "TNE") {
 			
 			tarifa = 200;
-			
+			//sino la tarifa es normal , es decir, 600
 		} else {
 			
 			tarifa = 600;
